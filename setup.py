ae68feb7b51161712ae191b0f2c8cbf5425ed989
barakme <barakme@gmail.com>
2015-01-04 10:37:51 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='mist-monitor',
+    version='1.0.0',
+    packages=['mist', 'mist_monitor'],
+    url='https://www.github.com/barakm/mist-monitor',
+    license='',
+    author='barakme',
+    author_email='barakme@gmail.com',
+    description='mist monitoring utilities',
+    install_requires=[
+        'cist.client'
+    ]
+)
